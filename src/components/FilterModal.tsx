f5ffb9c2fb469ec43d43a00f9f94270446b2e26c
Mary <marieshix98@gmail.com>
2022-12-12 23:53:22 +0300

    Add modal functionality

--- src/components/FilterModal.tsx
+++ src/components/FilterModal.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const FilterModal = () => {
-  return <div>FilterModal</div>;
+interface Props {
+  openModal: boolean;
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+  handleModal: () => void;
+}
+const FilterModal = ({ openModal, handleModal }: Props) => {
+  if (!openModal) {
+    return null;
+  }
+  return (
+    <div className="modal_wrapper">
+      <form className="modal_form">
+        <div className="close_modal" onClick={handleModal}>
+          <p>x</p>
+        </div>
+        <select>name</select>
+      </form>
+    </div>
+  );
 };
 
 export default FilterModal;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import FilterModal from "./FilterModal";
 
 interface Props {
   handleClick: React.Dispatch<React.SetStateAction<string>>;
   activeMenu?: string;
 }
 const Navbar = ({ handleClick, activeMenu }: Props) => {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const handleModal = () => {
+    setOpenModal((prev) => !prev);
+  };
   return (
-    <div className="nav_wrapper">
-      <h2 className="title">Rick and Morty</h2>
-      <div className="btn_wrapper">
-        <button className="btn" onClick={() => handleClick("Characters")}>
-          Characters
-        </button>
-        <button className="btn">
-          {activeMenu === "Characters"
-            ? "Filter by characters"
-            : " Filter by episodes"}
-        </button>
-        <button className="btn" onClick={() => handleClick("Episodes")}>
-          Episodes
-        </button>
+    <>
+      <div className="nav_wrapper">
+        <h2 className="title">Rick and Morty</h2>
+        <div className="btn_wrapper">
+          <button className="btn" onClick={() => handleClick("Characters")}>
+            Characters
+          </button>
+          <button className="btn" onClick={handleModal}>
+            {activeMenu === "Characters"
+              ? "Filter by characters"
+              : " Filter by episodes"}
+          </button>
+          <button className="btn" onClick={() => handleClick("Episodes")}>
+            Episodes
+          </button>
+        </div>
       </div>
-    </div>
+      {openModal && (
+        <FilterModal
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          handleModal={handleModal}
+        />
+      )}
+    </>
   );
 };
 
