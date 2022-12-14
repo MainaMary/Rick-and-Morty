b8865f091f0571476d7b6c61614e9c3a4db474dd
Mary <marieshix98@gmail.com>
2022-12-14 22:52:30 +0300

    Update characters response data

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -3,79 +3,47 @@ import { baseUrl } from "../api";
 import { useQuery } from "react-query";
 import axios from "axios";
 import CharactersCard from "./CharactersCard";
-import { BsSearch } from "react-icons/bs";
-const Characters = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [search, setSearch] = useState<string>("");
-  const [page, setPage] = useState<number>(1);
-  const [searchError, setSearchError] = useState<string>("");
-  const handleSearch = (e: any) => {
-    setSearch(e.target.value);
-  };
+import Search from "./Search";
+import useCounterHook from "../useCounterHook";
 
-  console.log(search, "search");
+const Characters = () => {
+  const { page, handleNext, handlePrev } = useCounterHook();
+
+  const [searchError, setSearchError] = useState<string>("");
+
   const fetchCharacters: any = async (page = 1): Promise<any> => {
-    //https://rickandmortyapi.com/api/character?page=1
     const response = await axios(`${baseUrl}/character?page=${page}`);
     return response?.data;
   };
-  const { data, error, isSuccess, isFetching, isError }: any = useQuery(
-    ["fetchCharacters", page],
-    () => fetchCharacters(page),
-    {
-      keepPreviousData: true,
-    }
-  );
-  console.log(page, "page");
+  const { data, error, isSuccess, isLoading, isFetching, isError }: any =
+    useQuery(
+      ["fetchCharacters", page],
+      () => fetchCharacters(page),
+
+      {
+        keepPreviousData: true,
+        cacheTime: 2000,
+      }
+    );
+
   if (isLoading) {
     <p>Loading...</p>;
   }
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    if (!search) {
-      setSearchError("Please search for a name");
-    }
-    setSearchError("");
-    const filterCharacters = async () => {
-      try {
-        const response = await axios(
-          `${baseUrl}/character/?name=${search}&status=alive`
-        );
-        console.log(response.data, "response");
-        setIsLoading(false);
-      } catch (error: any) {
-        console.log(error.message);
-        setIsLoading(false);
-      }
-      setSearch("");
-    };
-    filterCharacters();
-  };
-  useEffect(() => {}, []);
+  if (isError) {
+    return <p>{error.message}</p>;
+  }
+
   if (isError) return <p>{`An error has occured ${error.message}`}</p>;
-  console.log(page, "page");
-  console.log(searchError, "error");
+  console.log(data);
+  console.log(isLoading, isFetching);
   return (
     <>
       <div>{isFetching && <p>Loading...</p>}</div>
-      <div className="search-box">
-        <form className="form-modal" onSubmit={handleSubmit}>
-          <input
-            value={search}
-            onChange={handleSearch}
-            type="text"
-            name="name"
-            className="search-txt"
-            placeholder="Search by name..."
-          />
-          <button className="search-btn">
-            <BsSearch className="search-icon" />
-          </button>
-        </form>
-      </div>
+      <Search />
       <p style={{ textAlign: "center", color: "red", margin: "4px 0" }}>
         {searchError}
       </p>
+      <h2>All characters</h2>
       <div className="episode_Wrapper">
         {isSuccess && data.results.length
           ? data.results.map((item: any) => (
@@ -83,20 +51,21 @@ const Characters = () => {
             ))
           : null}
       </div>
-      <p>{`Page ${page}`}</p>
+      <p>{`Page ${page} out of ${data?.info.pages}`}</p>
       <div className="btn_wrapper">
         <button
           className="btn_pagination"
-          onClick={() => setPage((old) => Math.max(old - 1, 0))}
-          disabled={page === 0}
+          onClick={handlePrev}
+          disabled={page === 1}
         >
           Prev
         </button>
         <button
           className="btn_pagination"
+          disabled={page === 42}
           onClick={() => {
             console.log("hey");
-            setPage(page + 1);
+            handleNext();
           }}
           // Disable the Next Page button until we know a next page is available
         >
--- src/components/CharactersCard.tsx
+++ src/components/CharactersCard.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const CharactersCard = ({ item }: any) => {
+  console.log(item, "item");
+  const navigate = useNavigate();
+  const episode = item.episode[0].split("/").slice(-2).join(" ");
+
   return (
-    <div className="episode_Card">
+    <div
+      className="episode_Card"
+      onClick={() => navigate(`/singleCharacter/${item.id}`)}
+    >
       <img src={item.image} alt={item.name} className="image_card" />
       <h3>{item.name}</h3>
       <h3>{item.gender}</h3>
+      <h3>{episode}</h3>
     </div>
   );
 };
